1cbee7717637362600b355b33ebf034e1240ffaa
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2026-06-01 19:45:18 +0530

    Integrated both ( analytics + speed insights ) Vercel tools globally.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -53,6 +55,8 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
